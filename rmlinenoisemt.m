05a4f4652712b8638380041ed1d7775eb5b36d6e
Srivas Chennu <srivas@gmail.com>
2012-06-18 23:00:17 +0000

    Checked in Tue Jun 19 00:00:17 BST 2012
    git-svn-id: file:///Users/chennu/Dropbox/Sources/preprocessing@5 b849ebe8-00d1-41ce-8cde-7fe7c3505a83

---
+++ rmlinenoisemt.m
@@ -0,0 +1,11 @@
+function EEG = rmlinenoisemt(EEG,freq)
+
+if ~exist('freq','var') || isempty(freq)
+    freq = 50;
+end
+
+for c = 1:EEG.nbchan
+    for e = 1:EEG.trials
+        EEG.data(c,:,e) = cca_multitaper(EEG.data(c,:,e),EEG.srate,freq,EEG.srate);
+    end
+end
