e0f78441da323af0a9082f335cfb800b2b76b381
Srivas Chennu <srivas@gmail.com>
2013-09-17 23:00:21 +0000

    Checked in Wed Sep 18 00:00:20 BST 2013
    git-svn-id: file:///Users/chennu/Dropbox/Sources/preprocessing@21 b849ebe8-00d1-41ce-8cde-7fe7c3505a83

--- rejectic.m
+++ rejectic.m
@@ -54,11 +54,10 @@ if strcmp(param.skip,'off')
                 g = get(comptimefig, 'UserData');
                 badchan_new = cell2mat({g.eloc_file.badchan});
                 
-                plotcomp = param.sortorder;
-                for c = plotcomp
-                    if badchan_old(c) == 0 && (badchan_new(c) == 1 || EEG.reject.gcompreject(c) == 1)
+                for c = 1:length(badchan_old)
+                    if badchan_old(c) == 0 && (badchan_new(c) == 1 || EEG.reject.gcompreject(param.sortorder(c)) == 1)
                         g.eloc_file(c).badchan = 1;
-                    elseif badchan_old(c) == 1 && (badchan_new(c) == 0 || EEG.reject.gcompreject(c) == 0)
+                    elseif badchan_old(c) == 1 && (badchan_new(c) == 0 || EEG.reject.gcompreject(param.sortorder(c)) == 0)
                         g.eloc_file(c).badchan = 0;
                     end
                 end
