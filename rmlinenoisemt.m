12419a314e1cae42fa5e3fab13832a7468cc49e5
Srivas Chennu <srivas@gmail.com>
2014-02-13 00:00:24 +0000

    Checked in Thu Feb 13 00:00:24 GMT 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/preprocessing@24 b849ebe8-00d1-41ce-8cde-7fe7c3505a83

--- rmlinenoisemt.m
+++ rmlinenoisemt.m
@@ -4,8 +4,11 @@ if ~exist('freq','var') || isempty(freq)
     freq = 50;
 end
 
-for c = 1:EEG.nbchan
-    for e = 1:EEG.trials
-        EEG.data(c,:,e) = cca_multitaper(EEG.data(c,:,e),EEG.srate,freq,EEG.srate);
-    end
-end
+% for c = 1:EEG.nbchan
+%     for e = 1:EEG.trials
+%         EEG.data(c,:,e) = cca_multitaper(EEG.data(c,:,e),EEG.srate,freq,EEG.srate);
+%     end
+% end
+
+EEG = pop_cleanline(EEG,'LineFrequencies',[freq freq*2],'Bandwidth',2,'SignalType','Channels');
+ 
