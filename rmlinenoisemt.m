818ca4f15dfc7303f559bb9e33691e6724698c64
Srivas Chennu <srivas@gmail.com>
2014-02-20 00:00:21 +0000

    Checked in Thu Feb 20 00:00:21 GMT 2014
    git-svn-id: file:///Users/chennu/Dropbox/Sources/preprocessing@25 b849ebe8-00d1-41ce-8cde-7fe7c3505a83

--- rmlinenoisemt.m
+++ rmlinenoisemt.m
@@ -4,11 +4,16 @@ if ~exist('freq','var') || isempty(freq)
     freq = 50;
 end
 
+% %GCCA toolbox function
 % for c = 1:EEG.nbchan
 %     for e = 1:EEG.trials
 %         EEG.data(c,:,e) = cca_multitaper(EEG.data(c,:,e),EEG.srate,freq,EEG.srate);
 %     end
 % end
 
-EEG = pop_cleanline(EEG,'LineFrequencies',[freq freq*2],'Bandwidth',2,'SignalType','Channels');
- 
+% EEGLAB's cleanline function
+% EEG = pop_cleanline(EEG,'LineFrequencies',[freq freq*2],'Bandwidth',2,'SignalType','Channels');
+
+fprintf('Notch Filtering.\n');
+EEG = pop_eegfiltnew(EEG,freq-2,freq+2,[],1);
+EEG = pop_eegfiltnew(EEG,(freq*2)-2,(freq*2)+2,[],1);
